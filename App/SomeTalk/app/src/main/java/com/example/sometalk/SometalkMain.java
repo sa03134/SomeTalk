5f3cec912426011a088c1ec4ecb6ab7b8503f65b
ssongdda <70890888+ssongdda@users.noreply.github.com>
2020-12-16 17:12:15 +0900

    메인 페이지 디자인 수정 및 멘토 레이아웃 기초 작업

---
+++ App/SomeTalk/app/src/main/java/com/example/sometalk/SometalkMain.java
@@ -0,0 +1,14 @@
+package com.example.sometalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SometalkMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sometalk_main);
+    }
+}
