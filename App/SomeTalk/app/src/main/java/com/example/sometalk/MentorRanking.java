8f4f263bf7ab00a2a122070895b228aba07407f7
ssongdda <70890888+ssongdda@users.noreply.github.com>
2020-12-18 00:06:07 +0900

    멘토 랭킹, 대시보드 레이아웃 작업

---
+++ App/SomeTalk/app/src/main/java/com/example/sometalk/MentorRanking.java
@@ -0,0 +1,14 @@
+package com.example.sometalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MentorRanking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mentor_ranking);
+    }
+}
