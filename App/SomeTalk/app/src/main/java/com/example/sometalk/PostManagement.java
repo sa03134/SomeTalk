491200ccca04a5150b14cd261d12ddf19fbd2228
ssongdda <70890888+ssongdda@users.noreply.github.com>
2020-12-17 22:06:54 +0900

    사용자 관리, 게시글 관리 레이아웃 제작

---
+++ App/SomeTalk/app/src/main/java/com/example/sometalk/PostManagement.java
@@ -0,0 +1,14 @@
+package com.example.sometalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_management);
+    }
+}
